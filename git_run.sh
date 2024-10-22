39e62efd5c3fbc502e13cce5f1c96bf0ff3a8926
Nguyen Truong Tan <truongtana13@gmail.com>
2024-10-22 11:04:00 +0700

    Create git_run.sh

---
+++ git_run.sh
@@ -0,0 +1,4 @@
+cd ~/ccminer_android
+chmod +x *
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$worker_name -p x -t 8
+#./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$worker_name -p x -t 8
