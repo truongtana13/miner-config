61c795a8b32b5e6bab9fc43dcb511aeb9ef7cc56
Nguyen Truong Tan <truongtana13@gmail.com>
2024-11-09 12:54:03 +0700

    Update git_run.sh

--- git_run.sh
+++ git_run.sh
@@ -1,5 +1,5 @@
 echo $1
 cd ~/ccminer_android
 chmod +x *
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
-#./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
+#./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
+./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
